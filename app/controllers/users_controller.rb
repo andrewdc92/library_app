323945ced8d62f4a8baf4d60a2da95a0e4192a7a
Andrew Cordivari <andrewcord@williamvarismbp.gateway.sonic.net>
2016-10-04 19:43:04 -0700

    all get routes, post route, working

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def index
+    @users= User.all
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to root_path
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+end
