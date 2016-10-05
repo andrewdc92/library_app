57b25faae92f6e4631a6444ce115bdc5b8b3160b
Andrew Cordivari <andrewcord@William-Cordivaris-MacBook-Pro.local>
2016-10-05 10:25:16 -0700

    library get route working, controller/model created for libraries

---
+++ app/controllers/libraries_controller.rb
@@ -0,0 +1,10 @@
+class LibrariesController < ApplicationController
+  def index
+    @libraries = Library.all
+  end
+
+  def new
+    @library = Library.new
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   get '/users', to: 'users#index', as: "users"
+  get '/libraries', to: 'libraries#index'
   get '/users/new', to: 'users#new', as: 'new_user'
+  get '/libraries/new', to: 'libraries#new', as: 'new_library'
   post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show', as: "user"
   get '/login', to: 'sessions#new'
